13bcf5bbae28f9f6e720103ba1562cd6474de3d6
muhrik/majo/mario uhrik <433501@mail.muni.cz>
2018-05-23 11:52:27 +0200

    Wrote a new script computeSS_for_different_k_median.sh. Also, the constants in the models are now all undefined.

--- Script/computeSS_for_different_k_median.sh
+++ Script/computeSS_for_different_k_median.sh
@@ -1,13 +1,13 @@
 #compute steady state probabilities for various k values.
 #This script must remain in CZK_project/Script !!!
 #There is only one parameter - floating point number specifying the timeout.
-#Lambda is derived as 1/t.
+#Lambda is derived as (ln 2 / timeout).
 
 PRISM_PATH_FROM_SCRIPT="../../../../prismGSMP/prism-gsmp/prism/bin"
 MODEL_PATH_FROM_PRISM="../../../../CZK_project/Model"
 LOG_PATH_FROM_PRISM="../../../../CZK_project/Log"
 
-lambda=$(bc <<< "scale=10;1/$1")
+lambda=$(bc <<< "scale=10;0.693147181/$1")
 
 reNum='^[-+]?[0-9]+\.?[0-9]*$'
 if ! [[ $1 =~ $reNum ]]; then
@@ -17,8 +17,8 @@ fi
 
 cd '../Log'
 
-mkdir "t=$1"
-cd "t=$1"
+mkdir "t=$1_median"
+cd "t=$1_median"
 mkdir "explicit"
 cd "explicit"
 
@@ -26,17 +26,17 @@ cd $PRISM_PATH_FROM_SCRIPT
 #compute explicit phase type
 for k in 1 2 5 10 25 50 75 100 125 150 175 200 225 250 275 300
 do
-  ./prism -explicit -epsilon 1e-5 -maxiters 10000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_gsmp.sm" -ss -exportss "$LOG_PATH_FROM_PRISM/t=$1/explicit/ph_10_$k"
+  ./prism -explicit -epsilon 1e-5 -maxiters 100000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_gsmp.sm" -ss -exportss "$LOG_PATH_FROM_PRISM/t=$1_median/explicit/ph_10_$k"
 done
 
 #compute explicit event
-./prism -explicit -epsilon 1e-8 -maxiters 10000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$LOG_PATH_FROM_PRISM/t=$1/explicit/ev_10_k"
+./prism -explicit -epsilon 1e-8 -maxiters 100000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$LOG_PATH_FROM_PRISM/t=$1_median/explicit/ev_10_k"
 cd -
 
 #write a readme file for explicit computations
 touch readme.txt
 echo "t=$1" >> readme.txt
-echo "lambda=1/t" >> readme.txt
+echo "lambda=ln(2)/timeout" >> readme.txt
 echo "n=10" >> readme.txt
 echo "naming: ev/ph_n_k" >> readme.txt
 echo "steady state distributions using the GSMP (ctmc with phase type) explicit engine
@@ -50,14 +50,14 @@ cd $PRISM_PATH_FROM_SCRIPT
 #compute hybrid phase type
 for k in 1 2 5 10 25 50 75 100 125 150 175 200 225 250 275 300 500 1000 2000
 do
-  ./prism -hybrid -epsilon 1e-5 -maxiters 10000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$LOG_PATH_FROM_PRISM/t=$1/hybrid/ph_10_$k"
+  ./prism -hybrid -epsilon 1e-5 -maxiters 100000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$LOG_PATH_FROM_PRISM/t=$1_median/hybrid/ph_10_$k"
 done
 cd -
 
 #write a readme file for hybrid computations
 touch readme.txt
 echo "t=$1" >> readme.txt
-echo "lambda=1/t" >> readme.txt
+echo "lambda=ln(2)/timeout" >> readme.txt
 echo "n=10" >> readme.txt
 echo "naming: ph_n_k" >> readme.txt
 echo "steady state distributions using the CTMC with phase type on hybrid engine with Power method
