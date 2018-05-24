b7a385dc742fe8416ce976eedc89d13d3fd50fe1
Thomas_Mari <marithomas06@gmail.com>
2018-05-24 10:31:16 +0200

    computeSS k.sh done

--- Script/computeSS_for_different_k.sh
+++ Script/computeSS_for_different_k.sh
@@ -8,6 +8,8 @@ MODEL_PATH_FROM_PRISM="../../../../CZK_project/Model"
 OUTPUT_PATH_FROM_PRISM="../../../../CZK_project/Output"
 
 lambda=$(bc <<< "scale=10;1/$1")
+esp="1E-5"
+esp_precise="1E-8"
 
 reNum='^[-+]?[0-9]+\.?[0-9]*$'
 if ! [[ $1 =~ $reNum ]]; then
@@ -26,12 +28,15 @@ cd $PRISM_PATH_FROM_SCRIPT
 #compute explicit phase type
 for k in 1 2 5 10 25 50 75 100 125 150 175 200 225 250 275 300
 do
-  ./prism -explicit -epsilon 1e-5 -maxiters 10000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_gsmp.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ph_10_${k}_1E-5" > "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ph_10_${k}_1E-5.log"
+	echo explicit $k $esp
+  ./prism -explicit -epsilon ${esp} -maxiters 10000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_gsmp.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ph_10_${k}_${esp}" > "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ph_10_${k}_${esp}.log"
 done
 
 #compute explicit event
-./prism -explicit -epsilon 1e-8 -maxiters 10000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_1E-8" > "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_1E-8.log" 
-./prism -explicit -epsilon 1e-5 -maxiters 10000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_1E-5" > "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_1E-5.log"
+echo event $k $esp_precise
+./prism -explicit -epsilon ${esp_precise} -maxiters 10000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_${esp_precise}" > "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_${esp_precise}.log" 
+echo event $k $esp
+./prism -explicit -epsilon ${esp} -maxiters 10000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_${esp}" > "$OUTPUT_PATH_FROM_PRISM/t=$1/explicit/ev_10_k_${esp}.log"
 cd -
 
 #write a readme file for explicit computations
@@ -41,7 +46,7 @@ echo "lambda=1/t" >> readme.txt
 echo "n=10" >> readme.txt
 echo "naming: ev/ph_n_k_epsilon" >> readme.txt
 echo "steady state distributions using the GSMP (ctmc with phase type) explicit engine
-for termination epsilon 1.0E-5 for phase type, and termination epsilon 1.0E-8 and 1.0E-6 for event. 
+for termination epsilon ${esp} for phase type, and termination epsilon ${esp+} and ${esp} for event. 
 For each computation a full log file is also created with the extention .log" >> readme.txt
 
 cd "../"
@@ -52,7 +57,8 @@ cd $PRISM_PATH_FROM_SCRIPT
 #compute hybrid phase type
 for k in 1 2 5 10 25 50 75 100 125 150 175 200 225 250 275 300 500 1000 2000
 do
-  ./prism -hybrid -epsilon 1e-5 -maxiters 10000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/hybrid/ph_10_${k}_1E-5" > "$OUTPUT_PATH_FROM_PRISM/t=$1/hybrid/ph_10_${k}_1E-5.log"
+	echo hybrid $k $esp
+  ./prism -hybrid -epsilon 1e-5 -maxiters 10000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/t=$1/hybrid/ph_10_${k}_${esp}" > "$OUTPUT_PATH_FROM_PRISM/t=$1/hybrid/ph_10_${k}_${esp}.log"
 done
 cd -
 
@@ -63,6 +69,6 @@ echo "lambda=1/t" >> readme.txt
 echo "n=10" >> readme.txt
 echo "naming: ph_n_k_epsilon" >> readme.txt
 echo "steady state distributions using the CTMC with phase type on hybrid engine with Power method
-absolute termination criteria for termination epsilon 1.0E-5 for phase type.For each computation a full log file is also created with the extention .log" >> readme.txt
+absolute termination criteria for termination epsilon ${esp} for phase type.For each computation a full log file is also created with the extention .log" >> readme.txt
 
 
