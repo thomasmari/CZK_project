df6a007fa69e4cbd4d678adf08922e138ce96599
thomasmari <thomasmari06@gmail.com>
2018-06-28 09:05:25 -0400

    modification of scripts in computeSSfordifferentk change order of parameters and made hi more resistant to parameter with no matching also added a new script the same one but with fixed k in parameters

--- README.md
+++ README.md
@@ -1,64 +1,67 @@
 # CZK_project
 #Access output 
-To access the already cumputed results
-cd Output/
-The naming scheme used here for folders is 
-Output/t=TIMEOUT_KINDOFLAMBDA_EPSILON_KINDOFEPSILON/ENGINE
-where 
-	TIMEOUT is the production timeout. In the D/M/1/n queue the arrival process is following the Dirac(TIMEOUT) 
-	KINDOFLAMBDA is how is choosen the service time distribution rate. In the D/M/1/n queue the service distibution is EXP(LAMBDA)
-		KINDOFLAMBDA is choosen in {regular,median}
-			regular means that LAMBDA = 1/t
-			median means that LAMBDA = ln(2)/t
-	EPSILON is the wanted  precision of the computation, the expected form is 1E-[0-9]+ exemple : 1E-10
-		in PRISM it is the terminaition epsilon in command line
-		in Storm it is the --precision setting in command line
-	KINDOFEPSILON is how is choosen the actual presision in the the command line computation on both PRISM and Storm
-		KINDOFEPSILON is choosen in {constant,dynamic}
-			constant means that the epsilon used in computation will be always EPSILON 
-			dynamic means that the epsilon used in computation will changed with the value of PH fitting k as epsilon = EPSILON/k 
-			The ACTMC results will always be in constant KINDOFEPSILON folder because this is not a PH fitting model
+	To access the already cumputed results
+	The naming scheme used here for folders is 
+	Output/t=TIMEOUT_KINDOFLAMBDA_EPSILON_KINDOFEPSILON/ENGINE
+	where 
+		TIMEOUT is the production timeout. In the D/M/1/n queue the arrival process is following the Dirac(TIMEOUT) 
+			TIMEOUT is in seconds and should be written as [0-9]+.[0-9]+ 
+			example : 0.1 
+			The computed logs have all 0.1 for TIMEOUT 
+		KINDOFLAMBDA is how is choosen the service time distribution rate. In the D/M/1/n queue the service distibution is EXP(LAMBDA)
+			KINDOFLAMBDA is choosen in {regular,median}
+				regular means that LAMBDA = 1/t
+				median means that LAMBDA = ln(2)/t
+		EPSILON is the wanted  precision of the computation, the expected form is 1E-[0-9]+ exemple : 1E-10
+			in PRISM it is the terminaition epsilon in command line
+			in Storm it is the --precision setting in command line
+		KINDOFEPSILON is how is choosen the actual presision in the the command line computation on both PRISM and Storm
+			KINDOFEPSILON is choosen in {constant,dynamic}
+				constant means that the epsilon used in computation will be always EPSILON 
+				dynamic means that the epsilon used in computation will changed with the value of PH fitting k as epsilon = EPSILON/k 
+				The ACTMC results will always be in constant KINDOFEPSILON folder because this is not a PH fitting model
 
-	ENGINE is the engine used for the computation
-		ENGINE is choosen in {explicit,hybrid,storm}
-			explicit is the ACTMC computation on PRISM-GSMP and also the PH fitting CTMC SSP computed by PRISM-GSMP with explicit engine 
-			hybrid is the PH fitting CTMC SSP computed by PRISM with hybrid engine 
-			hybrid is the PH fitting CTMC SSP computed by Storm with sparse engine 
+		ENGINE is the engine used for the computation
+			ENGINE is choosen in {explicit,hybrid,storm}
+				explicit is the ACTMC computation on PRISM-GSMP and also the PH fitting CTMC SSP computed by PRISM-GSMP with explicit engine 
+				hybrid is the PH fitting CTMC SSP computed by PRISM with hybrid engine 
+				hybrid is the PH fitting CTMC SSP computed by Storm with sparse engine 
 
-The naming scheme used for files depends on the type of results
-	SSP results respect this naming scheme, 
-	MODEL_N_K_EPSILON
-	Where :
-		MODEL is choosen in {ev,ph}
-			ev for ACMC model
-			ph for PH fitting CTMC model
-		N is the maximum capacity of the queue. In the D/M/1/n queue we have N=n
-		K is the PH fitting parameter 
-			for Ph fitting CTMC it is written as [0-9]+
-			for ACTMC it is always k 
-			EPSILON is the same as the EPSILON above in the folder naming scheme
-		Those files contains the SSP results 
-			for PH fitting model in explicit or hybrid folders the order used is the Lexicographical order(qSize,i) (this file should contain N*k lines)
-			for PH fitting model in storm folder the order used is the Lexicographical order(qSize) (this file should contain N lines)
-			for ACTMC the order used is the Lexicographical order(qSize) (this file should contain N lines)
-	
-	SSP computation logs respect this naming scheme
-	MODEL_N_K_EPSILON.log
-		The naming is the same for SSP results 
-		Those files contains the logs for the computation, including command line and time of computation 
-			
-	SSP with post computation results respect this naming scheme, 
-	sumph_N_K_EPSILON
-	Where the naming scheme is the same that above 
-	those files will be only in explicit and hybrid folders for PH fitting CTMC model
-		Those files contains the sum of the SSP groupe by qSize which are display in the the Lexicographical order(qSize) 
+	The naming scheme used for files depends on the type of results
+		SSP results respect this naming scheme, 
+		MODEL_N_K_EPSILON
+		Where :
+			MODEL is choosen in {ev,ph}
+				ev for ACTMC model
+				ph for PH fitting CTMC model
+			N is the maximum capacity of the queue. In the D/M/1/n queue we have N=n
+			K is the PH fitting parameter 
+				for Ph fitting CTMC it is written as [0-9]+
+				for ACTMC it is always k 
+				EPSILON is the same as the EPSILON above in the folder naming scheme
+			Those files contains the SSP results 
+				for PH fitting model in explicit or hybrid folders the order used is the Lexicographical order(qSize,i) (this file should contain N*k lines)
+				for PH fitting model in storm folder the order used is the Lexicographical order(qSize) (this file should contain N lines)
+				for ACTMC the order used is the Lexicographical order(qSize) (this file should contain N lines)
+		
+		SSP computation logs respect this naming scheme
+		MODEL_N_K_EPSILON.log
+			The naming is the same for SSP results 
+			Those files contains the logs for the computation, including command line and time of computation 
+				
+		SSP with post computation results respect this naming scheme, 
+		sumph_N_K_EPSILON
+		Where the naming scheme is the same that above 
+		those files will be only in explicit and hybrid folders for PH fitting CTMC model
+			Those files contains the sum of the SSP groupe by qSize which are display in the the Lexicographical order(qSize) 
 
-	The following files are post computation used for the annalysis.   
-	MODEL_k_array
-		all PH fitting parameter k used in the folder order by increasing k
-	
-	MODEL_time_N
-		all time of computation of the SSP in the folder order by increasing k
+		The following files are post computation used for the annalysis.   
+		MODEL_k_array
+			all PH fitting parameter k used in the folder order by increasing k
+		
+		MODEL_time_N
+			all time of computation of the SSP in the folder order by increasing k
 
-	MODEL_eps_array
-		all exact epsilon used in computation used in the folder order by increasing k
+		MODEL_eps_array
+			all exact epsilon used in computation used in the folder order by increasing k
+
--- Script/computeSS.sh
+++ Script/computeSS.sh
@@ -1,46 +1,39 @@
 #PARAMETERS
 #$1 t(s) the timeout
-#$2 kinf_of_lambda according to t in {regular,median} regular mean lambda = 1/t, and median means t will be the median value of exp(lambda)
+#$2 lambda according to t in {regular,median} regular mean lambda = 1/t, and median means t will be the median value of exp(lambda)
 #$3 epsilon you want 
 #$4 the engine in {event,explicit,hybrid,storm}
 #$5 kind_of_epsilon in {dynamic, constant} for dynamic you have eps_computation = eps/k 	NOT NEEDED FOR EVENT ENGINE 
-#$6 the PH fitting parameter k 																NOT NEEDED FOR EVENT ENGINE
+#$6 the PH fitting parameter k in {event,explicit,hybrid,storm}								NOT NEEDED FOR EVENT ENGINE
 #compute steady state probabilities for various k values.
 #This script must remain in CZK_project/Script !!!
 # median means Lambda is derived as (ln 2 / timeout).
 
-#Path Setting
-PRISM_PATH_FROM_OUTPUT="../../../../prism-gsmp/prism/bin"
-CLASSIC_PRISM_PATH_FROM_OUTPUT="../../../../prism-4.4-src/bin"
-MODEL_PATH_FROM_PRISM="../../../CZK_project/Model"
-MODEL_PATH_FROM_CLASSIC_PRISM="../../CZK_project/Model"
-OUTPUT_PATH_FROM_PRISM="../../../CZK_project/Output"
-OUTPUT_PATH_FROM_CLASSIC_PRISM="../../CZK_project/Output"
-
 #Here you can set the sequence of k computed
 sequence=$(seq 1 1 9; seq 10 10 90; seq 100 100 900; seq 1000 1000 4000; seq 5000 5000 50000);
 
-#Help option
+
 if [ "$1" == "help" ]; then
-	echo '5 argument are expected :'
+	echo '4(event)-6(other engine) argument are expected :'
 	echo '$1 is the the timeout in second, the one we choose to run tests was 0.1'
-	echo '$2 kind_of_lambda according to t in {regular,median}'
+	echo '$2 lambda according to t in {regular,median}'
 	echo '   regular : lambda = 1/t'
 	echo '   median : t will be the median value of the exponential distribution exp(lambda) (ln(2)/t)'
 	echo '$3 is the the precision epsilon in [1-9]+{E-}[1-9]+ format, in prism it is the termination epsilon for power method, in storm it set the --epsilon option (1E-6 is the default value for both PRISM and Storm)'
-	echo '$4 the engine in {event,explicit,hybrid,storm} that will do the computation'
+	echo '$4 kind_of_epsilon in {dynamic, constant} for dynamic you have eps_computation = eps/k '
+	echo '   constant : the precision epsilon will remain the same for every computations'
+	echo '   dynamic : the precision epsilon will depend on k eps_computation = eps/k'
+	
+	echo '$5 the engine in {event,explicit,hybrid,storm} that will do the computation'
 	echo '   event is the ACTMC model checker'
 	echo '   explicit is the ACTMC model checker for only CTMC model'
 	echo '   hybrid is the PRISM model checker with hybrid engine'
-	echo '   storm is the Storm model checker with sparse engine'	
-	echo '$5 kind_of_epsilon in {dynamic, constant} for dynamic you have eps_computation = eps/k '
-	echo '   constant : the precision epsilon will remain the same for every computations'
-	echo '   dynamic : the precision epsilon will depend on k eps_computation = eps/k'
+	echo '   storm is the Storm model checker with sparse engine'
 	echo 'Result :'
 	echo 'It will compute logs and outputs for the specified parameters for a fixed range of k from 1 to 5000 with exponential steps'
 	exit 1
 	fi
-	
+
 #check parameter
 reNum='^[-+]?[0-9]+\.?[0-9]*$'
 if ! [[ $1 =~ $reNum ]]; then
@@ -60,40 +53,46 @@ if [ -z "$3" ]
     exit 1
 fi
 
-if [ -z "$4"  ]
+if [ -z "$5" ]
   then
     echo "engine not supplied, choose in {event,explicit,hybrid}"
     exit 1
 fi
 
-if [ -z "$5"] && [ ! $4 == "event" ];
+if [ -z "$4" ]
   then
-    echo "kind of epsilon not supplied for other engine that event, choose in {dynamic,constant}"
+    echo "kind of epsilon not supplied, choose in {dynamic,constant}"
     exit 1
 fi
 
 
 
 #Constant computation
-timeout="$1"			
-kindOfLambda="$2" 								#Read lamda
-epsilon=$(awk -v a="$3" 'BEGIN{print (a)}')	#Read epsilon
-engine="$4"
-kindOfEpsilon="$5"								#Read kind_of_epsilon
-#k="$6"									#PH fitting parameter k
+engine="$5"
+koe="$5" 
+eps=$(awk -v a="$3" 'BEGIN{print (a)}')
+echo $eps
+eps_precise="1E-10"
+
+
+PRISM_PATH_FROM_OUTPUT="../../../../prism-gsmp/prism/bin"
+CLASSIC_PRISM_PATH_FROM_OUTPUT="../../../../prism-4.4-src/bin"
+MODEL_PATH_FROM_PRISM="../../../CZK_project/Model"
+MODEL_PATH_FROM_CLASSIC_PRISM="../../CZK_project/Model"
+OUTPUT_PATH_FROM_PRISM="../../../CZK_project/Output"
+OUTPUT_PATH_FROM_CLASSIC_PRISM="../../CZK_project/Output"
+
 
 
 #lambda and path setting
 ln_2="0.6931471805599453094" #20 digits
-if [ $kindOfLambda == "regular" ]; then
+if [ "$2" == "regular" ]; then
 	lambda=$(echo "scale=20;1/$1" | bc)
-elif [ $kindOfLambda == "median" ]; then
-	lambda=$(echo "scale=20;${ln_2}/$1" | bc)
+	path="t=$1_regular_$3_$4"
 else
-	exit 1
+	lambda=$(echo "scale=20;${ln_2}/$1" | bc)
+	path="t=$1_median_$3_$4"
 fi
-
-path="t=${timeout}_${kindOfLambda}_${epsilon}_${kindOfEpsilon}"
 echo lambda = $lambda
 echo path = $path
 
@@ -119,25 +118,22 @@ fi
 
 if [ $engine == "event" ]; then
 	cd $PRISM_PATH_FROM_OUTPUT
-	echo engine epsilon_computation
+	echo 	engine 	epsilon_computation
 	#compute explicit event
 	echo event $eps
-	./prism -explicit -epsilon ${epsilon} -maxiters 100000000 -power -absolute -const timeout=$timeout,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/${path}/explicit/ev_10_k_${epsilon}" > "$OUTPUT_PATH_FROM_PRISM/${path}/explicit/ev_10_k_${epsilon}.log"
+	./prism -explicit -epsilon ${eps} -maxiters 100000000 -power -absolute -const timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/timeoutqueue.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/${path}/explicit/ev_10_k_${eps}" > "$OUTPUT_PATH_FROM_PRISM/${path}/explicit/ev_10_k_${eps}.log"
 	cd -
 	
 elif [ $engine == "explicit" ]; then
 	cd $PRISM_PATH_FROM_OUTPUT
-	echo engine k     epsilon_computation epsilon 
-	for k in $sequence
-		do
-			if [ $kindOfEpsilon == "dynamic" ]; then
-				eps_k=$(awk -v e="$epsilon" -v k0="$k" 'BEGIN{print (e / k0)}')
-			else
-				eps_k=$epsilon
-			fi
-			echo explicit $k $eps_k eps=$epsilon 
-			./prism -explicit -epsilon ${eps_k} -maxiters 100000000 -power -absolute -const k=$k,timeout=$timeout,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_gsmp.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/${path}/${engine}/ph_10_${k}_${epsilon}" > "$OUTPUT_PATH_FROM_PRISM/${path}/${engine}/ph_10_${k}_${epsilon}.log"
-		done
+	echo 	engine k 	epsilon_computation	epsilon 
+	if [ "$4" == "dynamic" ]; then
+		eps_k=$(awk -v e="$eps" -v kk="$k" 'BEGIN{print (e / kk)}')
+	else
+		eps_k=$eps
+	fi
+	echo explicit $k $eps_k eps=$eps 
+	./prism -explicit -epsilon ${eps_k} -maxiters 100000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_gsmp.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/${path}/${engine}/ph_10_${k}_${eps}" > "$OUTPUT_PATH_FROM_PRISM/${path}/${engine}/ph_10_${k}_${eps}.log"
 	cd -
 	#write a readme file for explicit computations
 	touch readme.txt
@@ -153,20 +149,16 @@ elif [ $engine == "explicit" ]; then
 elif [ $engine == "hybrid" ]; then
 	#~ cd $PRISM_PATH_FROM_OUTPUT
 	cd $CLASSIC_PRISM_PATH_FROM_OUTPUT
-	echo 	engine k 	epsilon_computation	epsilon 
+	echo engine k     epsilon_computation epsilon 
 	#compute hybrid phase type
-	for k in $sequence;
-	do
-		if [ $kindOfEpsilon == "dynamic" ]; then
-			eps_k=$(awk -v e="$epsilon" -v k0="$k" 'BEGIN{print (e / k0)}')
-		elif [ $kindOfEpsilon == "constant" ]; then
-			eps_k=$epsilon
-		else
-			exit 1
-		fi
-		echo 	hybrid 	$k 	$eps_k 	$epsilon
-		./prism -hybrid -epsilon ${eps_k} -maxiters 100000000 -power -absolute -const k=$k,timeout=$timeout,lambda=$lambda "$MODEL_PATH_FROM_CLASSIC_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$OUTPUT_PATH_FROM_CLASSIC_PRISM/${path}/${engine}/ph_10_${k}_${epsilon}" > "$OUTPUT_PATH_FROM_CLASSIC_PRISM/${path}/${engine}/ph_10_${k}_${epsilon}.log"
-	done
+	if [ "$4" == "dynamic" ]; then
+		eps_k=$(awk -v e="$eps" -v k0="$k" 'BEGIN{print (e / k0)}')
+	else
+		eps_k=$eps
+	fi
+	echo hybrid $k $eps_k $eps
+	#~ ./prism -hybrid -epsilon ${eps_k} -maxiters 100000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$OUTPUT_PATH_FROM_PRISM/${path}/${engine}/ph_10_${k}_${eps}" > "$OUTPUT_PATH_FROM_PRISM/${path}/${engine}/ph_10_${k}_${eps}.log"
+	./prism -hybrid -epsilon ${eps_k} -maxiters 100000000 -power -absolute -const k=$k,timeout=$1,lambda=$lambda "$MODEL_PATH_FROM_CLASSIC_PRISM/queue_withptf_ctmc.sm" -ss -exportss "$OUTPUT_PATH_FROM_CLASSIC_PRISM/${path}/${engine}/ph_10_${k}_${eps}" > "$OUTPUT_PATH_FROM_CLASSIC_PRISM/${path}/${engine}/ph_10_${k}_${eps}.log"
 	cd -
 	#write a readme file for hybrid computations
 	touch readme.txt
@@ -181,20 +173,17 @@ elif [ $engine == "hybrid" ]; then
 elif [ $engine == "storm" ]; then
 	#~ cd $PRISM_PATH_FROM_OUTPUT
 	cd $CLASSIC_PRISM_PATH_FROM_OUTPUT
-	echo	engine k	epsilon_computation	epsilon 
+	echo engine k     epsilon_computation epsilon 
+	
 	#compute hybrid phase type
-	for k in $sequence;
-		do
-			if [ $kindOfEpsilon == "dynamic" ]; then
-				eps_k=$(awk -v e="$epsilon" -v k0="$k" 'BEGIN{print (e / k0)}')
-			elif [ $kindOfEpsilon == "constant" ]; then
-				eps_k=$epsilon
-			else
-				exit 1
-			fi
-			echo storm $k $eps_k $epsilon
-			storm --prism "$MODEL_PATH_FROM_CLASSIC_PRISM/queue_withptf_ctmc.sm" --prop "$MODEL_PATH_FROM_CLASSIC_PRISM/storm_prop.csl" -e sparse -pc --precision "$eps_k" --abstraction:precision "$eps_k" --constants k="$k",timeout="$timeout",lambda="$lambda" > "$OUTPUT_PATH_FROM_CLASSIC_PRISM/${path}/${engine}/ph_10_${k}_${epsilon}.log"
-	done
+	if [ "$4" == "dynamic" ]; then
+		eps_k=$(awk -v e="$eps" -v kk="$k" 'BEGIN{print (e / kk)}')
+	else
+		eps_k=$eps
+	fi
+	echo storm $k $eps_k $eps
+	storm --prism "$MODEL_PATH_FROM_CLASSIC_PRISM/queue_withptf_ctmc.sm" --prop "$MODEL_PATH_FROM_CLASSIC_PRISM/storm_prop.csl" -e sparse -pc --precision "$eps_k" --abstraction:precision "$eps_k" --constants k="$k",timeout="$1",lambda="$lambda" > "$OUTPUT_PATH_FROM_CLASSIC_PRISM/${path}/${engine}/ph_10_${k}_${eps}.log"
+	
 	cd -
 	#write a readme file for hybrid computations
 	touch readme.txt
@@ -204,4 +193,13 @@ elif [ $engine == "storm" ]; then
 	echo "naming: ph_n_k_epsilon" >> readme.txt
 	echo "steady state distributions using the CTMC with phase type on hybrid engine with Power method
 	absolute termination criteria for termination epsilon 1.0E-5 for phase type." >> readme.txt
+
+
 fi
+
+
+
+
+
+
+
