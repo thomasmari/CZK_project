56692db728eab4ba79ded96c4622dd341c18d084
Thomas <marithomas06@gmail.com>
2018-07-02 13:12:44 +0200

    addind a new version of tw0phaseservice with inhibition condition, this could explained the 1E-1 precision

---
+++ benchmark/devel/twoPhaseService/twoPhaseService_ACTMC_v2.pm
@@ -0,0 +1,23 @@
+gsmp
+
+// rates
+const double PREEMPTION_RATE = 0.5; // rate at the system enters a pre-emption state discontinuing the service
+const double CONTINUATION_RATE = 2.0; // rate at the system goes back into a state that allows service
+
+module twoPhaseService
+
+event service1Time  = dirac(1.5); // time it takes to finish the first part of the job
+event service2Time  = dirac(0.5); // time it takes to finish the second part of the job
+		
+	preemption : [0..1] init 0; // 0- continuing service, 1- not continuing service
+	stage : [0..1] init 0; // 0- doing the first part of the job, 1- doing the second part of the job
+
+	[discontinue] (preemption=0) -> PREEMPTION_RATE: (preemption'=1);
+
+	[continue] (preemption=1) -> CONTINUATION_RATE: (preemption'=0);
+
+	[firstStage] (stage=0) && (preemption=1) --service1Time-> (stage'=1);
+
+	[secondStage] (stage=1) && (preemption=1) --service2Time-> (stage'=0);
+
+endmodule
